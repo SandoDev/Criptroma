63b1c48e99ae2afc96b6b53deff8d32d72d91615
SandoDev <camilo.sandoval@zinobe.com>
2020-09-23 11:19:43 -0500

    new resources and first model of app gui

--- README.md
+++ README.md
@@ -16,7 +16,14 @@ para hacer dibujos de los criptogramas creados
   * pygame
 
 
-# TODO
+## TODO
 1. que reciba un tento por teclado
 2. que trasnforme el texto en el criptograma
 3. que se pueda exportar el criptograma como imagen
+4. guardar los criptogramas generados en base de datos
+
+
+## TODO-code
+1. crear folder criptroma
+2. move various files to past folder
+3. main.py in root of project
--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import tkinter as tk
+import constants
+
+
+class Application:
+    def __init__(self, window):
+        self.wind = window
+        self.wind.title(constants.NAME_APP)
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Creating a Frame container
+        frame = tk.LabelFrame(self.wind, text='Crear un nuevo Criptroma')
+        frame.grid(row=0, column=0, columnspan=3, pady=10)
+
+        # Text input
+        text_area = tk.Text(frame, height=10, width=50)
+        text_area.grid(row=1, column=0)
+
+        # Button Generate
+        bt_generate = tk.Button(frame, text='Generar')
+        bt_generate.grid(row=3, columnspan=2, sticky=tk.W+tk.E)
+
+
 def main():
-    print("hello world")
+    window = tk.Tk()
+    application = Application(window=window)
+    window.mainloop()
 
 
 if __name__ == "__main__":
